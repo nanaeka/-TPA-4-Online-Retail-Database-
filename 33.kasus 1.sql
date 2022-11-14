8b76d5caa9e8e954f404bc1d7d4a7f9e435fecb9
Nana eka <114203205+nanaeka@users.noreply.github.com>
2022-11-15 00:41:22 +0700

    TPA 4

--- 33.kasus 1.sql
+++ 33.kasus 1.sql
@@ -1,2 +1,2 @@
-INSERT INTO `order` (`id`,`id_category`,`amount`,`order_name`,`order_address`,`order_phone_number`,`order_date`,`order_status`) VALUES (1, 1, 82.000,'nana eka','sdr 1 damai 2 no 6',081331913558, 2022-11-04, 1);
+INSERT INTO `order` (`id`,`id_category`,`amount`,`order_name`,`order_address`,`order_phone_number`,`order_date`,`order_status`) VALUES (1, 1, 82.000,'nana eka','komplek kpum',089676425728, 2022-11-04, 1);
 INSERT INTO `order_details` (`id`,`id_product`,`id_order`) VALUES (1, 1, 1),(2,1,1),(3,1,1);
