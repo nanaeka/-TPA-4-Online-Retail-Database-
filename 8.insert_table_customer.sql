91476ed04c5189d5c42d4a77afcde2e3b73af570
Nana eka <114203205+nanaeka@users.noreply.github.com>
2022-11-15 00:42:53 +0700

    TPA 4

--- 8.insert_table_customer.sql
+++ 8.insert_table_customer.sql
@@ -1,1 +1,1 @@
-INSERT INTO `customer` (`id`, `full_name`, `email`, `password`, `address`, `phone_number`) VALUES (1, 'nana eka', 'nanasembirin2019@gmail.com', 'nanaeka', 'sdr1 damai 2 no 6', '081331913558');
+INSERT INTO `customer` (`id`, `full_name`, `email`, `password`, `address`, `phone_number`) VALUES (1, 'nana eka', 'nanasembirin2019@gmail.com', 'nanaeka', 'komplek kpum', '089676425728');
