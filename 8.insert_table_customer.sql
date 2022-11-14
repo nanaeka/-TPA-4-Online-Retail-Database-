86fcaba2f2a29574b7a0a6244c1d9b5e47c3ddef
Nana eka <114203205+nanaeka@users.noreply.github.com>
2022-11-14 23:57:10 +0700

    Update 8.insert_table_customer.sql

--- 8.insert_table_customer.sql
+++ 8.insert_table_customer.sql
@@ -1,1 +1,1 @@
-INSERT INTO `customer` (`id`, `full_name`, `email`, `password`, `address`, `phone_number`) VALUES (1, 'Muhammad zaid abdillah', 'zaidabdillah18@gmail.com', 'zaid123', 'sdr1 damai 2 no 6', '081331913558');
+INSERT INTO `customer` (`id`, `full_name`, `email`, `password`, `address`, `phone_number`) VALUES (1, 'nana eka', 'nanasembirin2019@gmail.com', 'nanaeka', 'sdr1 damai 2 no 6', '081331913558');
